119be8c9700b6cde6d0dae80b4c076f73ac2a6b2
unknown <smcim2520@gmail.com>
2026-09-07 11:55:42 +0700

    fix: FAvcd1

--- src/WarehousePage.jsx
+++ src/WarehousePage.jsx
@@ -351,6 +351,7 @@ function ReceiveForm({ drugs, warehouseDeptId, onReceived, editTarget, minMaxByN
             <option value="เม็ด">เม็ด</option>
             <option value="ยาฉีด (Ampoule)">ยาฉีด (Ampoule)</option>
             <option value="ยาฉีด (Vial)">ยาฉีด (Vial)</option>
+            <option value="แคปซูล ">แคปซูล</option>
             <option value="ซอง (Powder)">ซอง (Powder)</option>
             <option value="ขวด">ขวด</option>
           </select>
